0f97c2317cba59804e0bc1e5a1e486a525fe4cae
Junaed Hossain <hossain.junaed11@gmail.com>
2021-07-03 17:00:42 -0500

    Created routing between Main page and My List page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import mainPage from './pages/mainPage.jsx'
+import myList from './pages/myList.jsx'
 import * as MovieAPI from './MovieAPI';
 
 class App extends React.Component {
   render = () => {
     return (
-      <h1>hello world!</h1>
+      <div>
+        <h1>hello world!</h1>        
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={mainPage} />
+          <Route path="/my-list" component={myList} />
+        </Switch>
+      </BrowserRouter>
+      </div>        
     );
   }
 }
